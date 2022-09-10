5ad2023d726c66e88f18c274f43340b8526b0818
Maksym Chesherov <chesherow22d@gmail.com>
2022-09-10 15:20:26 +0300

    refactored and added keybord input

--- skript.js
+++ skript.js
@@ -1,62 +1,86 @@
 let value, operate, length;
+let screen = document.querySelector(".screen");
 
 let numbers = document.querySelectorAll(".hoverNumber");
 for(let i = 0; i < numbers.length; i++){
     let id = numbers[i].id;
         numbers[i].addEventListener("click", id =>{
-            console.log(document.querySelector(".screen").textContent.length)
-            document.querySelector(".screen").textContent += [i] + "";
+            screen.textContent += [i] + "";
         });
-}
+};
+
+document.addEventListener("keydown", (e) =>{
+    if(e.key == "1" || e.key == "2" || e.key == "3" || e.key == "4" || e.key == "5" || e.key == "6" ||
+        e.key == "7" || e.key == "8" || e.key == "9" || e.key == "0"){
+        screen.textContent += e.key;
+    }
+});
+
+let signs = document.querySelectorAll(".sign");
+for(let i = 0; i < signs.length; i++){
+    let sign = signs[i].textContent;
+    let id2 = signs[i].id;
+
+    document.addEventListener("keydown", (e) =>{
+        if(e.key == sign){
+            value = screen.textContent;
+            operate = signs[i].innerHTML + "";
+            screen.textContent = "";
+        }else{
+            return;
+        }
+    });
+};
 
 document.querySelector("#plus").addEventListener("click", plus);
 function plus() {
-    value = document.querySelector(".screen").textContent;
+    value = screen.textContent;
     operate = '+';
-    document.querySelector(".screen").textContent = "";
+    screen.textContent = "";
 }
 
 document.querySelector("#minus").addEventListener("click", minus);
 function minus() {
-    value = document.querySelector(".screen").textContent;
+    value = screen.textContent;
     operate = '-';
-    document.querySelector(".screen").textContent = "";
+    screen.textContent = "";
 }
 
 document.querySelector("#multiplication").addEventListener("click", multiplication);
 function multiplication() {
-    value = document.querySelector(".screen").textContent;
+    value = screen.textContent;
     operate = '*';
-    document.querySelector(".screen").textContent = "";
+    screen.textContent = "";
 }
 
 document.querySelector("#division").addEventListener("click", division);
 function division() {
-    value = document.querySelector(".screen").textContent;
+    value = screen.textContent;
     operate = '/';
-    document.querySelector(".screen").textContent = "";
+    screen.textContent = "";
 }
 
 document.querySelector("#squareRoot").addEventListener("click", squareRoot);
 function squareRoot() {
-    value = document.querySelector(".screen").textContent;
+    value = screen.textContent;
     operate = '√';
     value =  Math.pow(Number(value), .5);
-    document.querySelector(".screen").textContent = value.toFixed(8);
+    screen.textContent = value.toFixed(8);
 }
 
 document.querySelector("#remove").addEventListener("click", remove);
 function remove() {
-    document.querySelector(".screen").textContent = "";
+    screen.textContent = "";
 }
 
 document.querySelector("#reset").addEventListener("click", reset);
 function reset() {
-    length = document.querySelector(".screen").textContent;
-    document.querySelector(".screen").textContent = length.substring(0, length.length - 1)
+    length = screen.textContent;
+    screen.textContent = length.substring(0, length.length - 1)
 }
 
 document.querySelector("#equals").addEventListener("click", equals);
+document.querySelector("#equals").addEventListener("keydown", equals);
 function equals() {
     if (operate == "+") {
         document.querySelector(".screen").textContent = Number(value) + Number(document.querySelector(".screen").textContent);
@@ -68,7 +92,7 @@ function equals() {
         if (document.querySelector(".screen").textContent === "0") {
             document.querySelector(".screen").textContent = "incorrect value";
         } else {
-            document.querySelector(".screen").textContent = (Number(value) / Number(document.querySelector(".screen").textContent)).toFixed(8);
+            document.querySelector(".screen").textContent = (Number(value) / Number(document.querySelector(".screen").textContent));
         }
     }
 }
